4b73136f7ed9aaed7428d71890867bb488d702a4
VIVEK <vkvillain2000@gmail.com>
2025-08-16 16:40:49 +0530

    third change

--- src/main.jsx
+++ src/main.jsx
@@ -3,10 +3,10 @@ import ReactDOM from "react-dom/client";
 import {BrowserRouter} from "react-router-dom";
 import App from "./App";
 
-ReactDOM.createRoot(document.getElementById("root"));
-root.render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <BrowserRouter basename="/React-Routing/">
     <App />
   </BrowserRouter>
-);
+  );
+
 

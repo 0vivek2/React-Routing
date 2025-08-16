439c33769883febfcce39cd3542b4eca40707a28
VIVEK <vkvillain2000@gmail.com>
2025-08-16 16:48:25 +0530

    second change

--- src/Home.jsx
+++ src/Home.jsx
@@ -2,7 +2,7 @@ function Home() {
   return <div>
     <h1>Hello!! <br />Welcome to Home Page</h1>
     <div style={{ display: "flex", justifyContent: "center", alignItems: "center"}}>
-    <img style={{maxHeight:"65vh", maxWidth:"80vw"}} src="{`${import.meta.env.BASE_URL}\hello_img.jpg" alt="Hello Image"/>
+    <img style={{maxHeight:"65vh", maxWidth:"80vw"}} src=".\hello_img.jpg" alt="Hello Image"/>
     </div>
   </div>
 }
